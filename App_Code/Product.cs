bd4b02d7757d3a8efbe8f7fda30aea149e53f00c
MaDo1989 <33927366+MaDo1989@users.noreply.github.com>
2018-01-15 12:50:50 +0200

    Initial commit

---
+++ App_Code/Product.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Product
+/// </summary>
+public class Product
+{ XMLServices xml = new XMLServices();
+    public Product()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public List<Product> getProducts()
+    {
+        return xml.getProducts();
+    }
+
+    public Product(int _productID, string _title, double _price, int _inventory, string _imgPath, bool _avail)
+    {
+        id = _productID;
+        title = _title;
+        price = _price;
+        inventory = _inventory;
+        imagePath = _imgPath;
+        available = _avail;
+    }
+
+    private bool available;
+
+    public bool Available
+    {
+        get { return available; }
+        set { available = value; }
+    }
+
+    private int id;
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    private string title;
+
+    public string Title
+    {
+        get { return title; }
+        set { title = value; }
+    }
+
+    //public double discount(bool b)
+    //{
+    //    if (b)
+    //    {
+    //        return 0.2;
+    //    }
+    //    else
+    //    {
+    //        return 0.5;
+    //    }
+    //}
+
+    //public int insertNewProduct(int category, string title, string imgPath, double price, int inventory)
+    //{
+
+    //    return db.insertProduct(category, title, imgPath, price, inventory);
+    //}
+
+    private string imagePath;
+
+    public string ImagePath
+    {
+        get { return imagePath; }
+        set { imagePath = value; }
+    }
+
+    //public DataTable getAllSales(string input)
+    //{
+    //    return db.getAllSales(input);
+    //}
+
+    private double price;
+
+    //public void updateInventory(int id, int inventory)
+    //{
+    //    db.updateInventory(id, inventory);
+    //}
+
+    //public List<Product> getProductsList()
+    //{
+    //    DataTable dt = getAllProducts();
+    //    List<Product> ls = new List<Product>();
+
+    //    foreach (DataRow row in dt.Rows)
+    //    {
+    //        if (!Convert.ToBoolean(row[6].ToString()))
+    //        {
+    //            continue;
+    //        }
+    //        Product p = new Product(int.Parse(row[0].ToString()), row[2].ToString(), double.Parse(row[4].ToString()), int.Parse(row[5].ToString()), row[3].ToString(), Convert.ToBoolean(row[6].ToString()));
+    //        ls.Add(p);
+    //    }
+    //    return ls;
+    //}
+
+    public double Price
+    {
+        get { return price; }
+        set { price = value; }
+    }
+
+    private int inventory;
+
+    public int Inventory
+    {
+        get { return inventory; }
+        set { inventory = value; }
+    }
+
+    //public void insertSale(int id, double price, int inventory, string userName, DateTime date, int paymentMethod)
+    //{
+    //    db.insertSale(id, price, inventory, userName, date, paymentMethod);
+    //}
+
+    //public Category category = new Category();
+
+    //private Dictionary<string, string> attributes;
+
+    //public Dictionary<string, string> Attributes
+    //{
+    //    get { return attributes; }
+    //    set { attributes = value; }
+    //}
+
+
+    //public Product(int _categoryId, int _id, string _title, string _imagePath, double _price, int _inventory, Dictionary<string, string> _attr)
+    //{
+    //    category.Id = _categoryId;
+    //    Id = _id;
+    //    Title = _title;
+    //    ImagePath = _imagePath;
+    //    Price = _price;
+    //    Inventory = _inventory;
+    //    Attributes = _attr;
+
+    //}
+
+    //public int checkAvaiable(string inventory, int id)
+    //{
+    //    return db.checkAvaiable(inventory, id);
+    //}
+
+    //public void updateProduct(string id, string inventory, bool available)
+    //{
+    //    db.updateProduct(id, inventory, available);
+    //}
+
+    //public Product getProduct(int productId)
+    //{
+    //    //call the method getProduct from DBService
+    //    return null;
+    //}
+
+    //public DataTable getAllProducts()
+    //{
+    //    return db.GetAllProducts();
+    //}
+
+
+}
---
+++ App_Code/XMLServices.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using System.Xml;
+
+
+/// <summary>
+/// Summary description for XMLServices
+/// </summary>
+public class XMLServices
+{
+    public XMLServices()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public List<Product> getProducts()
+    {
+        String xmlFile = "ProductsDB.xml";
+
+        // Define a settings which will be used by the reader 
+        XmlReaderSettings xmlSettings = new XmlReaderSettings();
+        xmlSettings.IgnoreWhitespace = true;
+        xmlSettings.IgnoreComments = true;
+
+      //  int TeamMemberCount = 0;
+
+        List<Product> lp = new List<Product>();
+
+        // using(  ) is used when accessing a resource, it implements the try/catch model behind the scene and
+        // takes care of disposing the resource after usage
+        using (XmlReader XmlRdr = XmlReader.Create(xmlFile, xmlSettings))
+        {
+            while (XmlRdr.Read())
+            {
+                if (XmlRdr.NodeType == XmlNodeType.Element && XmlRdr.LocalName == "Item")
+                {
+                   
+                        Product p = new Product(int.Parse(XmlRdr.GetAttribute(0)), XmlRdr.GetAttribute(1), double.Parse(XmlRdr.GetAttribute(2)), int.Parse(XmlRdr.GetAttribute(3)), XmlRdr.GetAttribute(4),Convert.ToBoolean(int.Parse(XmlRdr.GetAttribute(5))));
+                    lp.Add(p);
+                }
+            }
+        }
+        return lp;
+
+       // Response.Write(String.Format("There are {0} team members in the department", TeamMemberCount.ToString()));
+    }
+}
